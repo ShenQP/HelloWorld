988079d02308d7b56be52685bb4bea3bf42c89bc
ShenQP <sqp6750@163.com>
2021-06-09 09:06:59 +0800

    提交文件

---
+++ src/Class1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Class1 {
+    /*
+    public void main(String arg){
+        Scanner sc=new Scanner(System.in);
+        String str1=sc.nextLine();
+        //String str2=sc.nextLine();
+        System.out.println(str1);
+        //System.out.println(str2);;
+
+
+    }
+
+     */
+
+}
---
+++ src/HelloClass.java
@@ -0,0 +1,11 @@
+import java.lang.module.FindException;
+import java.util.*;
+
+public class HelloClass {
+    public static void main(String[] args) {
+        String str="  hello  world!  ";
+        System.out.println("The length of str:"+str.length());
+        System.out.println(str.trim());
+
+    }
+}
